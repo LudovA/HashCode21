4d3077cb04082b11134098cb9340188a52b1a1d2
LudovA <ludovanalst@gmail.com>
2021-02-23 13:35:53 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# %% IMPORTING MODULES
+from utils import *
+
+
+# %% DEFINE CONSTANTS
+
+
+# %% FUNCTIONS
+def main():
+    print("Hello world.")
+
+
+# %% MAIN
+if __name__ == '__main__':
+    main()

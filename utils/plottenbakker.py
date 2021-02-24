b57dc803057a77b4778fbea0cbb6db2a71c42230
LudovA <ludovanalst@gmail.com>
2021-02-24 20:26:53 +0100

    Script to plot stats added, currently supports histograms

---
+++ utils/plottenbakker.py
@@ -0,0 +1,16 @@
+#%% IMPORTING MODULES
+import matplotlib.pyplot as plt
+import os
+
+
+#%% DEFINING FUNCTIONS
+def plot_histogram(values, bins, title='Histogram', output_dir=os.path.join(os.path.dirname(os.getcwd()), 'statistics/plots/')):
+    plt.hist(values, bins=bins)
+    plt.title(title)
+    plt.xlabel('Values')
+    plt.ylabel('Count')
+    plt.show()
+
+    if output_dir:
+        output_path = os.path.join(output_dir, title + '.png')
+        plt.savefig(output_path)
